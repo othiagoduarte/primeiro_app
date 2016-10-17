02d93dbb1e8376be7a69a3dcb8455dff6fe7fe6f
Thiago Duarte <thiago.duarte@outlook.com>
2016-10-17 20:57:19 -0200

    Banco de dados

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 
-var $Host	= "m7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-var $Username = "ihj9mzw3pfbb32o1";
-var $Password = "xsopcit1hvr3r6yq";
-var $Port =	3306;
+$Host	= "m7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$Username = "ihj9mzw3pfbb32o1";
+$Password = "xsopcit1hvr3r6yq";
+$Port =	3306;
 
 mysqli_connect($Host, $Username, $Password, "mysql") or die(mysqli_error());
 echo "Connected to MySQL<br/>";
