95a6a3ef13d284bdb873da56c71463af66a88827
Thiago Duarte <thiago.duarte@outlook.com>
2016-10-17 21:01:47 -0200

    Base de dados

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $Host	= "m7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $Username = "ihj9mzw3pfbb32o1";
 $Password = "xsopcit1hvr3r6yq";
 $Port =	3306;
+$db = "sfn5si6huyqqshuv";
 
-mysqli_connect($Host, $Username, $Password, "mysql") or die(mysqli_error());
+mysqli_connect($Host, $Username, $Password, $db) or die(mysqli_error());
 echo "Connected to MySQL<br/>";
