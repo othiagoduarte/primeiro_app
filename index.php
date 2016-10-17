21b6b45d0ec25cca5669a7968fce8f478477dead
Thiago Duarte <thiago.duarte@outlook.com>
2016-10-17 20:55:03 -0200

    Adicionar banco

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php 
-phpinfo();
+
+var $Host	= "m7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+var $Username = "ihj9mzw3pfbb32o1";
+var $Password = "xsopcit1hvr3r6yq";
+var $Port =	3306;
+
+mysqli_connect($Host, $Username, $Password, "mysql") or die(mysqli_error());
+echo "Connected to MySQL<br/>";
